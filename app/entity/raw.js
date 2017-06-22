0e669928ac8617815e5019b17d08c382dbf4f937
Dominic Barnes <dominic@dbarnes.info>
2017-06-22 09:09:20 -0700

    use highlight.js to make raw view easier to read

--- app/entity/raw.js
+++ app/entity/raw.js
@@ -1,13 +1,18 @@
 /** @jsx element */
 
 import element from 'virtual-element'
+import hljs from 'highlight.js'
+
+export function afterMount (component, el, setState) {
+  hljs.highlightBlock(el)
+}
 
 export function render ({ props }) {
   const { entity } = props
 
   return (
-    <div class='c-code c-code--multiline'>
-      {JSON.stringify(entity.toObject(), null, 2)}
-    </div>
+    <pre>
+      <code class='json'>{JSON.stringify(entity.toObject(), null, 2)}</code>
+    </pre>
   )
 }
